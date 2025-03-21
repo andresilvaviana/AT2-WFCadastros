9170a8ab35979e7d811acbd9e9e6c10231aec941
ANDRE SILVA VIANA <andre.silva.viana.1995@gmail.com>
2025-03-20 21:08:24 -0300

    Adicionar arquivos de projeto.

---
+++ AT2-WFCadastros/ClassCadastroCategoria.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AT2_WFCadastros
+{
+    public enum ETipoStatus
+    {
+        Ativo,
+        Inativo,
+    }
+
+    public class CadastroCategoria
+    {
+        public CadastroCategoria(int codigo, string? nome, string? descricao, ETipoStatus eTipoStatus)
+        {
+            Codigo = codigo;
+            Nome = nome;
+            Descricao = descricao;
+            ETipoStatus = eTipoStatus;
+        }
+
+        public CadastroCategoria() { }
+
+        public static List<CadastroCategoria> ListaCategoria = new List<CadastroCategoria>();
+
+        public int Codigo { get; set; }
+        public string? Nome { get; set; }
+        public string? Descricao { get; set; }
+        public ETipoStatus ETipoStatus { get; set; }
+
+        public static List<CadastroCategoria> ObterLista()
+        {
+            return ListaCategoria;
+        }
+    }
+}
